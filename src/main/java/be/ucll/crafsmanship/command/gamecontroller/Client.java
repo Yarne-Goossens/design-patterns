cf7036117bdc30a3e4bccd3c8c372041b0888217
Yarne-Goossens <yarne.goossens@student.ucll.be>
2025-11-10 18:54:40 +0100

    Exercises

--- src/main/java/be/ucll/crafsmanship/command/gamecontroller/Client.java
+++ src/main/java/be/ucll/crafsmanship/command/gamecontroller/Client.java
@@ -5,20 +5,21 @@ public class Client {
         Receiver receiver = new Receiver();
 
         Command crossCommand = new CrossButtonCommand(receiver);
-//        Command triangleCommand = new TriangleButtonCommand(receiver);
-//        Command squareCommand = new SquareButtonCommand(receiver);
-//        Command circleCommand = new CircleButtonCommand(receiver);
-//
-//        PS5Controller controller = new PS5Controller();
-//        controller.setCrossButton(crossCommand);
-//        controller.setTriangleButton(triangleCommand);
-//        controller.setSquareButton(squareCommand);
-//        controller.setCircleButton(circleCommand);
-//
-//        // Simulate button presses
-//        controller.pressCross();
-//        controller.pressTriangle();
-//        controller.pressSquare();
-//        controller.pressCircle();
+        Command triangleCommand = new TriangleButtonCommand(receiver);
+        Command squareCommand = new SquareButtonCommand(receiver);
+        Command circleCommand = new CircleButtonCommand(receiver);
+
+        PS5Controller controller = new PS5Controller();
+        controller.setCrossButton(crossCommand);
+        controller.setTriangleButton(triangleCommand);
+        controller.setSquareButton(squareCommand);
+        controller.setCircleButton(circleCommand);
+
+        // Simulate button presses
+        controller.pressCross();
+        controller.pressTriangle();
+        controller.pressSquare();
+        controller.pressTriangle();
+        controller.pressCircle();
     }
 }
--- src/main/java/be/ucll/crafsmanship/command/gamecontroller/PS5Controller.java
+++ src/main/java/be/ucll/crafsmanship/command/gamecontroller/PS5Controller.java
@@ -1,5 +1,42 @@
 package be.ucll.crafsmanship.command.gamecontroller;
 
 public class PS5Controller {
-    // todo
+    Command crossButton;
+    Command squareButton;
+    Command circleButton;
+    Command triangleButton;
+
+    public PS5Controller() {}
+
+    public void setCrossButton(Command crossButton) {
+        this.crossButton = crossButton;
+    }
+
+    public void setSquareButton(Command squareButton) {
+        this.squareButton = squareButton;
+    }
+
+    public void setCircleButton(Command circleButton) {
+        this.circleButton = circleButton;
+    }
+
+    public void setTriangleButton(Command triangleButton) {
+        this.triangleButton = triangleButton;
+    }
+
+    public void pressCross() {
+        crossButton.execute();
+    }
+
+    public void pressTriangle() {
+        triangleButton.execute();
+    }
+
+    public void pressSquare() {
+        squareButton.execute();
+    }
+
+    public void pressCircle() {
+        circleButton.execute();
+    }
 }
